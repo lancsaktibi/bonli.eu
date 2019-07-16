277cfd0e8129cc9d477460a4d54c94e5e3dcfca8
Tibor Lancsak <lancsaktibi@gmail.com>
2019-07-16 21:34:48 +0200

    untrack node_modules

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 
 // use router and routes file
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 // any other routes: pass over to the angular app
 app.use((req, res, next) => {
